da4eae529f833d960894ecd2d732f3a3816613f6
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-03-13 11:54:44 +0700

    change amount value

--- src/Models/Deal.php
+++ src/Models/Deal.php
@@ -51,7 +51,8 @@ class Deal extends Model
     public function setAmountAttribute($value)
     {
         if (isset($value)) {
-            $this->attributes['amount'] = $value * 100;
+            // $this->attributes['amount'] = $value * 100;
+            $this->attributes['amount'] = $value;
         } else {
             $this->attributes['amount'] = null;
         }
--- src/Models/Lead.php
+++ src/Models/Lead.php
@@ -49,7 +49,8 @@ class Lead extends Model
     public function setAmountAttribute($value)
     {
         if (isset($value)) {
-            $this->attributes['amount'] = $value * 100;
+            // $this->attributes['amount'] = $value * 100;
+            $this->attributes['amount'] = $value;
         } else {
             $this->attributes['amount'] = null;
         }
