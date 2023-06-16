e038a922746949f391d512b3df406a2185d71fb2
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-06-16 23:47:35 +0700

    update

--- resources/views/components/card-header.blade.php
+++ resources/views/components/card-header.blade.php
@@ -1,6 +1,9 @@
 <div class="card-header">
-    <h3 class="card-title mr-auto m-0">{{ $title }}</h3> 
-    <div class="card-toolbar d-flex flex-row align-items-center">
-        {{ $actions }}
+    <div class="d-flex justify-content-between">
+        <h3 class="card-title mr-auto m-0">{{ $title }}</h3> 
+        <div class="card-toolbar d-flex flex-row align-items-center">
+            {{ $actions }}
+        </div>  
     </div>
+    
 </div>
