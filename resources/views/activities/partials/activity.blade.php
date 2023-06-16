0302caa4336dae7122e60ba08c5047b271b86a49
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-06-16 20:56:16 +0700

    added people

--- resources/views/activities/partials/activity.blade.php
+++ resources/views/activities/partials/activity.blade.php
@@ -1,3 +1,4 @@
+@props(['activity','people' => null])
 @switch($activity->recordable_type)
     @case('VentureDrake\LaravelCrm\Models\Note')
         @livewire('note',[
@@ -16,17 +17,20 @@
         @break
     @case('VentureDrake\LaravelCrm\Models\Call')
         @livewire('call',[
-        'call' => $activity->recordable
+        'call' => $activity->recordable,
+        'people' => $people,
         ], key('call_'.$activity->recordable->id))
         @break
     @case('VentureDrake\LaravelCrm\Models\Meeting')
         @livewire('meeting',[
-        'meeting' => $activity->recordable
+        'meeting' => $activity->recordable,
+        'people' => $people,
         ], key('meeting_'.$activity->recordable->id))
         @break
     @case('VentureDrake\LaravelCrm\Models\Lunch')
         @livewire('lunch',[
-        'lunch' => $activity->recordable
+        'lunch' => $activity->recordable,
+        'people' => $people
         ], key('lunch_'.$activity->recordable->id))
         @break
 @endswitch
