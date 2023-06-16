ec211429c7ec6294836511d43fff2045762a4191
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-06-16 23:44:35 +0700

    change component card header

--- resources/views/components/card-header.blade.php
+++ resources/views/components/card-header.blade.php
@@ -1,1 +1,6 @@
-<div class="card-header"><h3 class="card-title float-left m-0">{{ $title }}</h3> <span class="float-right">{{ $actions }}</span></div>
+<div class="card-header">
+    <h3 class="card-title mr-auto m-0">{{ $title }}</h3> 
+    <div class="card-toolbar d-flex flex-row align-items-center">
+        {{ $actions }}
+    </div>
+</div>
