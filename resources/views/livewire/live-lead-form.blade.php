f201d3fb2e4cea5f40b86ea6dd0e96e905235e3c
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-06-18 22:37:15 +0700

    check people variable

--- resources/views/livewire/live-lead-form.blade.php
+++ resources/views/livewire/live-lead-form.blade.php
@@ -297,7 +297,12 @@
                                 url: "/crm/organisations/" +  item.value + "/autocomplete",
                                 cache: false
                             }).done(function( data ) {
-                                people = data.people;
+                                if(typeof people ==='undefined') {
+                                    let people = data.people;
+                                } else {
+                                    people = data.people;
+                                }
+                                
                                 $('.autocomplete-organisation').find('input[name="line1"]').val(data.address_line1);
                                 $('.autocomplete-organisation').find('input[name="line2"]').val(data.address_line2);
                                 $('.autocomplete-organisation').find('input[name="line3"]').val(data.address_line3);
