e75cbc89c6332b5c6928a46dbf25c3c3abf202a8
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-07-28 22:12:47 +0700

    new layout for dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
     <div class="container-content">
+        @if(auth()->user()->hasRole(['Manager','Owner']))
         <div class="row">
             @hasleadsenabled
             <div class="col-sm mb-4">
-                <div class="card">
+                <div class="card mb-4">
                     <div class="card-header">
                         <h4 class="card-title float-left m-0">{{ ucfirst(__('laravel-crm::lang.leads')) }}</h4>
                     </div>
@@ -19,7 +20,7 @@
             @endhasleadsenabled
             @hasdealsenabled
             <div class="col-sm mb-4">
-                <div class="card">
+                <div class="card mb-4">
                     <div class="card-header">
                         <h4 class="card-title float-left m-0">{{ ucfirst(__('laravel-crm::lang.deals')) }}</h4>
                     </div>
@@ -53,7 +54,132 @@
                 </div>
             </div>
         </div>
+        @endif
+        <div class="row">
+
+            <div class="col-sm-4">
+                @hasleadsenabled
+                <div class="card border-primary mb-4">
+                    <div class="card-header text-white bg-primary">
+                        <h4 class="card-title float-left m-0">Your {{ ucfirst(__('laravel-crm::lang.leads')) }}</h4>
+                    </div>
+                    <div class="card-body">
+                        <h2>{{ $myTotalLeadsCount ?? 0 }}</h2>
+                        <small>{{ ucfirst(__('laravel-crm::lang.total_leads')) }}</small>
+                    </div>
+                </div>
+                @endhasleadsenabled
+                @hasdealsenabled
+                <div class="card text-white bg-primary mb-4">
+                    <div class="card-header">
+                        {{--                        <h4 class="card-title float-left m-0">Your {{ ucfirst(__('laravel-crm::lang.deals')) }}</h4>--}}
+                        <ul class="nav nav-pills card-header-pills">
+                            <li class="nav-item">
+                                <button class="nav-link text-white active" id="mydeal-tab" data-toggle="tab" data-target="#mydeal" type="button" role="tab" aria-controls="mydeal" aria-selected="true">Your Deals</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link text-success" id="mydealwon-tab" data-toggle="tab" data-target="#mydealwon" type="button" role="tab" aria-controls="mydealwon" aria-selected="true">Won Deals</button>
+                            </li>
+                            <li class="nav-item">
+                                <button class="nav-link text-danger" id="mydeallost-tab" data-toggle="tab" data-target="#mydeallost" type="button" role="tab" aria-controls="mydeallost" aria-selected="true">Lost Deals</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="mydeal" role="tabpanel" aria-labelledby="mydeal-tab">
+                                <h2>{{ $myTotalDealsCount ?? 0 }}</h2>
+                                <small>{{ ucfirst(__('laravel-crm::lang.total_deals')) }}</small>
+                            </div>
+                            <div class="tab-pane" id="mydealwon" role="tabpanel" aria-labelledby="mydealwon-tab">
+                                <h2>{{ $myTotalWonDealsCount ?? 0 }}</h2>
+                                <small>{{ ucfirst(__('laravel-crm::lang.total_deals')) }}</small>
+                            </div>
+                            <div class="tab-pane" id="mydeallost" role="tabpanel" aria-labelledby="mydeallost-tab">
+                                <h2>{{ $myTotalLostDealsCount ?? 0 }}</h2>
+                                <small>{{ ucfirst(__('laravel-crm::lang.total_deals')) }}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endhasdealsenabled
+            </div>
+
+            <div class="col-sm-4">
+                <div class="card border-primary mb-4">
+                    <div class="card-header text-white bg-primary">
+                        <h4 class="card-title float-left m-0">Your {{ ucfirst(__('laravel-crm::lang.people')) }}</h4>
+                    </div>
+                    <div class="card-body">
+                        <h2>{{ $myTotalPeopleCount ?? 0 }}</h2>
+                        <small>{{ ucfirst(__('laravel-crm::lang.total_people')) }}</small>
+                    </div>
+                </div>
+                <div class="card border-primary mb-4">
+                    <div class="card-header text-white bg-primary">
+                        <h4 class="card-title float-left m-0">Your {{ ucfirst(__('laravel-crm::lang.organizations')) }}</h4>
+                    </div>
+                    <div class="card-body">
+                        <h2>{{ $myTotalOrganisationsCount ?? 0 }}</h2>
+                        <small>{{ ucfirst(__('laravel-crm::lang.total_organizations')) }}</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card text-white bg-success">
+                    <div class="card-header">
+                        <h4 class="card-title m-0"><i class="fa fa-birthday-cake"></i> {{ ucfirst('People Birthday') }}</h4>
+                    </div>
+                    <div class="card-body">
+
+                            <div class="row">
+                                <div class="col-sm">
+                                    @if(count($personBirthday) > 0)
+                                        @foreach($personBirthday as $person)
+                                        <div class="media {{ (!$loop->last) ? 'mb-3' : null }}">
+                                            <span class="fa fa-user fa-2x mr-3 border rounded-circle p-2 align-self-center" aria-hidden="true"></span>
+                                            <div class="media-body">
+                                                <h4 class="mt-1 mb-0">{{ $person->name }}</h4>
+                                                @php
+                                                    $organisation = $person->contacts()->where('entityable_type','LIKE','%Organisation%')->first();
+                                                    $organisationName = '';
+
+                                                    if($organisation) {
+                                                        $organisationName = $organisation->entityable->name;
+                                                    }
+                                                @endphp
+                                                <p class="mb-0">{{  \Carbon\Carbon::parse($person->birthday)->format('d-m-Y') }}.</p>
+                                                <p class="mb-0"><span class="badge badge-info text-white">{{ $organisationName }}</span> </p>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                    @else
+                                        {{ ucfirst('Nobody has a birthday this month') }}
+                                    @endif
+                                </div>
+                                <div class="col-sm">
+                                    <div class="m-auto" style="width:100px;height:100px;">
+                                        <div class="row h-100 align-items-center justify-content-center bg-info rounded-circle text-center">
+                                            <div class="col-12">
+                                                <h4>
+                                                    {{ \Carbon\Carbon::now()->monthName }}
+                                                </h4>
+                                                <h4>{{ \Carbon\Carbon::now()->year }}</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+
     <div class="row">
         <div class="col-sm-8 mb-4">
             <div class="card">
@@ -66,7 +192,8 @@
             </div>
         </div>
         <div class="col-sm mb-4">
-            <div class="card">
+
+            <div class="card mb-4">
                 <div class="card-header">
                     <h4 class="card-title m-0">{{ ucfirst(__('laravel-crm::lang.users_online')) }}</h4>
                 </div>
@@ -82,6 +209,7 @@
                     @endforeach
                 </div>
             </div>
+
         </div>
     </div>
     
--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -59,12 +59,22 @@ class DashboardController extends Controller
         foreach ($deals as $deal) {
             $datasheet[$deal->created_at->format('d/m/Y')]["daily"]["deals"]++;
         }
+
+        // Birthday person
+        $birthdays = Person::where('user_owner_id', auth()->user()->id)->whereMonth('birthday',Carbon::now()->month)->get();
         
         return view('laravel-crm::index', [
             'totalLeadsCount' => Lead::count(),
+            'myTotalLeadsCount' => Lead::where('user_owner_id', auth()->user()->id)->whereNull('converted_at')->count(),
             'totalDealsCount' => Deal::count(),
+            'myTotalDealsCount' => Deal::where('user_owner_id', auth()->user()->id)->count(),
+            'myTotalWonDealsCount' => Deal::where(['user_owner_id' => auth()->user()->id, 'closed_status' => 'won'])->count(),
+            'myTotalLostDealsCount' => Deal::where(['user_owner_id' => auth()->user()->id, 'closed_status' => 'lost'])->count(),
             'totalPeopleCount' => Person::count(),
+            'myTotalPeopleCount' => Person::where('user_owner_id', auth()->user()->id)->count(),
             'totalOrganisationsCount' => Organisation::count(),
+            'myTotalOrganisationsCount' => Organisation::where('user_owner_id', auth()->user()->id)->count(),
+            'personBirthday' => $birthdays,
             'usersOnline' => $usersOnline,
             'createdLast14Days' => json_encode($datasheet),
         ]);
