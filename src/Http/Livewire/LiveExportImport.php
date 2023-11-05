2c51de749bcbe4f4cee9d13d76aa7ec704800795
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-11-05 21:37:17 +0700

    update

--- src/Http/Livewire/LiveExportImport.php
+++ src/Http/Livewire/LiveExportImport.php
@@ -2,6 +2,8 @@
 
 namespace VentureDrake\LaravelCrm\Http\Livewire;
 
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -12,9 +14,57 @@ class LiveExportImport extends Component
     use NotifyToast;
     use WithFileUploads;
 
+    public $batchId;
+    public $exporting = false;
+    public $exportFinished = false;
+    public $model;
+    public $owner;
+
+    public $file;
+
     public function mount()
     {
         
+    }
+
+    public function export()
+    {
+        $this->exporting = true;
+        $this->exportFinished = false;
+
+        $this->validate([
+            'model' => 'required',
+            'owner' => 'required|exists:users,id'
+        ]);
+
+        $batch = Bus::batch([
+            new ExportJob(),
+        ])->dispatch();
+
+        $this->batchId = $batch->id;
+    }
+
+    public function getExportBatchProperty()
+    {
+        if (!$this->batchId) {
+            return null;
+        }
+
+        return Bus::findBatch($this->batchId);
+    }
+
+    public function downloadExport()
+    {
+        return Storage::download('public/transactions.csv');
+    }
+
+    public function updateExportProgress()
+    {
+        $this->exportFinished = $this->exportBatch->finished();
+
+        if ($this->exportFinished) {
+            $this->exporting = false;
+        }
     }
 
     public function render()
