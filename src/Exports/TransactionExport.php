baca062fc0174368f954733af3d77084ae3bbc50
Yusuf Mulhajat <uchup_bell@yahoo.com>
2023-11-18 09:54:15 +0700

    added PeopleSheet

--- src/Exports/TransactionExport.php
+++ src/Exports/TransactionExport.php
@@ -5,8 +5,9 @@ namespace VentureDrake\LaravelCrm\Exports;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 use VentureDrake\LaravelCrm\Exports\Sheets\LabelsSheet;
-use \VentureDrake\LaravelCrm\Exports\Sheets\LeadSheet;
-use \VentureDrake\LaravelCrm\Exports\Sheets\DealSheet;
+use VentureDrake\LaravelCrm\Exports\Sheets\PeopleSheet;
+use VentureDrake\LaravelCrm\Exports\Sheets\LeadSheet;
+use VentureDrake\LaravelCrm\Exports\Sheets\DealSheet;
 use VentureDrake\LaravelCrm\Exports\Sheets\OrganisationsSheet;
 use VentureDrake\LaravelCrm\Exports\Sheets\OrganisationTypesSheet;
 use VentureDrake\LaravelCrm\Exports\Sheets\UsersSheet;
@@ -42,6 +43,13 @@ class TransactionExport implements WithMultipleSheets
                 $sheets = [
                     new DealSheet($this->owner),
                     new LabelsSheet(),
+                ];
+                break;
+                
+            case 'VenturaDrake\LaravelCrm\Models\People':
+                $sheets = [
+                    new PeopleSheet($this->owner),
+                    new LabelsSheet()
                 ];
                 break;
 
